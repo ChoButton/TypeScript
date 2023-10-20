61e46b17bce6ec279da48d747140be4dbbea06b4
DESKTOP-3GH2IHM\SAMSUNG <chosy0716@gmail.com>
2023-10-20 18:02:50 +0900

    조승연 : ch07 공부중

---
+++ ch05/pureDelete-test.ts
@@ -0,0 +1,10 @@
+import { pureDelete } from "./pureDelete";
+
+const mixedArray: object[] = [
+  [], {name:'Jack'}, {name:'Jane', age: 32}, ['description']
+]
+const objectOnly: object[] = pureDelete(
+  mixedArray,
+ (val) => Array.isArray(val)
+)
+console.log(mixedArray, objectOnly)
